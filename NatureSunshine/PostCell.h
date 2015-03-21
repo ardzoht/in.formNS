d58a4c830034fdc7741a679312be6df6532457e9
Armando Colomo Baray <acb0654@gmail.com>
2015-03-21 13:14:17 -0700

    added config cells for wall VC

---
+++ NatureSunshine/PostCell.h
@@ -0,0 +1,13 @@
+//
+//  PostCell.h
+//  NatureSunshine
+//
+//  Created by alumno on 3/21/15.
+//  Copyright (c) 2015 Alex&Dave. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PostCell : UITableViewCell
+
+@end
---
+++ NatureSunshine/PostCell.m
@@ -0,0 +1,13 @@
+//
+//  PostCell.m
+//  NatureSunshine
+//
+//  Created by alumno on 3/21/15.
+//  Copyright (c) 2015 Alex&Dave. All rights reserved.
+//
+
+#import "PostCell.h"
+
+@implementation PostCell
+
+@end
