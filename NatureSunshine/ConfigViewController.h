190eded828c46aae8dd7db81d5871ab931c8cda5
Armando Colomo Baray <acb0654@gmail.com>
2015-04-20 00:12:32 -0600

    added configuration view controller
    decided to use a simple page view controller instead of a page view
    controller because there’s not much time to do it

--- NatureSunshine/ConfigViewController.h
+++ NatureSunshine/ConfigViewController.h
@@ -2,12 +2,20 @@
 //  ConfigViewController.h
 //  NatureSunshine
 //
-//  Created by alumno on 4/18/15.
+//  Created by alumno on 4/19/15.
 //  Copyright (c) 2015 Alex&Dave. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ConfigViewController : UIViewController
+@interface ConfigViewController : UIViewController <UIPickerViewDelegate, UIPickerViewDataSource>
+
+@property (weak, nonatomic) IBOutlet UITextField *heightText;
+@property (weak, nonatomic) IBOutlet UITextField *weightText;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *waterTrack;
+@property (weak, nonatomic) IBOutlet UITextView *objectiveText;
+@property (weak, nonatomic) IBOutlet UIPickerView *coaches;
+@property (weak, nonatomic) IBOutlet UIButton *send;
+
 
 @end
--- NatureSunshine/ConfigViewController.m
+++ NatureSunshine/ConfigViewController.m
@@ -2,11 +2,12 @@
 //  ConfigViewController.m
 //  NatureSunshine
 //
-//  Created by alumno on 4/18/15.
+//  Created by alumno on 4/19/15.
 //  Copyright (c) 2015 Alex&Dave. All rights reserved.
 //
 
 #import "ConfigViewController.h"
+#import <Parse/Parse.h>
 
 @interface ConfigViewController ()
 
@@ -14,9 +15,19 @@
 
 @implementation ConfigViewController
 
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+
 }
 
 - (void)didReceiveMemoryWarning {
