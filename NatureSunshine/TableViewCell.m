2f6d26bad4ff414e4e7a40df8809149d4171d69a
DaveTheFinder <metalwarrior117@gmail.com>
2015-04-28 16:34:30 -0600

    Changes Tracker
    Well…

--- NatureSunshine/TableViewCell.m
+++ NatureSunshine/TableViewCell.m
@@ -19,20 +19,21 @@
 
 @implementation TableViewCell
 
-- (void)configUI:(NSIndexPath *)indexPath
-{
+
+- (void)configUI:(NSIndexPath *)indexPath{
     if (chartView) {
         [chartView removeFromSuperview];
         chartView = nil;
     }
     
     path = indexPath;
-    
+  
     chartView = [[UUChart alloc]initwithUUChartDataFrame:CGRectMake(10, 10, [UIScreen mainScreen].bounds.size.width-20, 150)
                                               withSource:self
                                                withStyle:indexPath.section==1?UUChartBarStyle:UUChartLineStyle];
     [chartView showInView:self.contentView];
 }
+
 
 - (NSArray *)getXTitles:(int)num
 {
@@ -44,159 +45,138 @@
     return xTitles;
 }
 
+
 #pragma mark - @required
+
 //横坐标标题数组
 - (NSArray *)UUChart_xLableArray:(UUChart *)chart
-{    
+{
     PFUser *currentUser = [PFUser currentUser];
     NSString *userN = [[NSString alloc] initWithFormat:@"%@", currentUser.username];
+    
     
     PFQuery *query = [PFQuery queryWithClassName:@"Water"];
     [query whereKey:@"username" equalTo:userN];
     [query selectKeys:@[@"nOunces"]];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            nEntries = [NSNumber numberWithUnsignedInteger:objects.count];
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
+    count = [query findObjects];
+    
     
     PFQuery *query1 = [PFQuery queryWithClassName:@"Calories"];
     [query1 whereKey:@"username" equalTo:userN];
     [query1 selectKeys:@[@"nCalories"]];
-    [query1 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            nEntries1 = [NSNumber numberWithUnsignedInteger:objects.count];
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
+    count1 = [query1 findObjects];
+    
     
     PFQuery *query2 = [PFQuery queryWithClassName:@"Weight"];
     [query2 whereKey:@"username" equalTo:userN];
     [query2 selectKeys:@[@"nPounds"]];
-    [query2 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            nEntries2 = [NSNumber numberWithUnsignedInteger:objects.count];
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
+    count2 = [query2 findObjects];
+    
+
+    nEntries = (NSInteger)count.count;
+    nEntries1 = (NSInteger)count1.count;
+    nEntries2 = (NSInteger)count2.count;
     
     
-    if (path.section==0) {
-        return [self getXTitles:[nEntries intValue]];
+    switch (path.section) {
+        case 0:
+            return [self getXTitles:nEntries];
+        case 1:
+            return [self getXTitles:nEntries1];
+        case 2:
+            return [self getXTitles:nEntries2];
+            
+        default:
+            break;
     }
-     else{
-         if(path.section==1){
-             return [self getXTitles:[nEntries1 intValue]];
-         }
-         
-         else{
-             return [self getXTitles:[nEntries2 intValue]];
-         }
-    }
-    return [self getXTitles:20];
+    
+    return [self getXTitles:nEntries];
 }
 
 
 //数值多重数组
 - (NSArray *)UUChart_yValueArray:(UUChart *)chart{
-    /*
-     NSArray *ary = @[@"22",@"44",@"15",@"40",@"42"];
-     NSArray *ary1 = @[@"22",@"54",@"15",@"30",@"42",@"77",@"43"];
-     NSArray *ary2 = @[@"76",@"34",@"54",@"23",@"16",@"32",@"17"];
-     NSArray *ary3 = @[@"3",@"12",@"25",@"55",@"52"];
-     NSArray *ary4 = @[@"23",@"42",@"25",@"15",@"30",@"42",@"32",@"40",@"42",@"25",@"33"]; */
-    
+  
     PFUser *currentUser = [PFUser currentUser];
-    NSString *userN = [[NSString alloc] initWithFormat:@"%@", currentUser.username];
-    
-    PFQuery *query = [PFQuery queryWithClassName:@"Water"];
-    [query whereKey:@"username" equalTo:userN];
-    [query selectKeys:@[@"nOunces"]];
-    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            ary = objects;
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
-    
-    PFQuery *query1 = [PFQuery queryWithClassName:@"Calories"];
-    [query1 whereKey:@"username" equalTo:userN];
-    [query1 selectKeys:@[@"nCalories"]];
-    [query1 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            ary1 = objects;
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
-    
-    PFQuery *query2 = [PFQuery queryWithClassName:@"Weight"];
-    [query2 whereKey:@"username" equalTo:userN];
-    [query2 selectKeys:@[@"nPounds"]];
-    [query2 findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
-        if (!error) {
-            ary2 = objects;
-        } else {
-            // Log details of the failure
-            NSLog(@"Error: %@ %@", error, [error userInfo]);
-        }
-    }];
+    NSLog(@"%@", currentUser.username);
     
     
-    if (path.section == 0) {
-           return @[ary];
+    PFQuery *myQuery = [PFQuery queryWithClassName:@"Water"];
+    [myQuery whereKey:@"username" equalTo:currentUser.username];
+    [myQuery selectKeys:@[@"nOunces"]];
+    ary = [myQuery findObjects];
+
+    PFQuery *myQuery1 = [PFQuery queryWithClassName:@"Calories"];
+    [myQuery1 whereKey:@"username" equalTo:currentUser.username];
+    [myQuery1 selectKeys:@[@"nCalories"]];
+    ary1 = [myQuery1 findObjects];
+    
+    PFQuery *myQuery2 = [PFQuery queryWithClassName:@"Weight"];
+    [myQuery2 whereKey:@"username" equalTo:currentUser.username];
+    [myQuery2 selectKeys:@[@"nPounds"]];
+    ary2 = [myQuery2 findObjects];
+    
+    
+    
+    NSString *h;
+    anAry = [[NSMutableArray alloc] init];
+    anAry1 = [[NSMutableArray alloc] init];
+    anAry2 = [[NSMutableArray alloc] init];
+    
+
+    for (int i = 0; i<ary.count; i++) {
+        h = [NSString stringWithFormat:@"%d", [ary[i] intValue]];
+        [anAry addObject:h];
     }
-    else{
-        if(path.section == 1){
-            return @[ary1];
-      }
-        else{
-            return @[ary2];
-        }
+    
+    for (int i = 0; i<ary1.count; i++) {
+        h = [NSString stringWithFormat:@"%d", [ary[i] intValue]];
+        [anAry1 addObject:h];
+    }
+    
+    for (int i = 0; i<ary2.count; i++) {
+        h = [NSString stringWithFormat:@"%d", [ary[i] intValue]];
+        [anAry2 addObject:h];
+    }
+    
+    switch (path.section) {
+        case 0:
+            return @[anAry];
+        case 1:
+            return @[anAry1];
+        case 2:
+            return @[anAry2];
+    
+        default:
+            return @[anAry,anAry1,anAry2];
     }
 }
 
+
 #pragma mark - @optional
+
 //颜色数组
+
 - (NSArray *)UUChart_ColorArray:(UUChart *)chart
 {
-    return @[UUGreen,UURed,UUBrown];
+   return @[UUGreen,UURed,UUBrown];
 }
+
 //显示数值范围
+
 - (CGRange)UUChartChooseRangeInLineChart:(UUChart *)chart
 {
-    if (path.section==0 && (path.row==0|path.row==1)) {
-        return CGRangeMake(60, 10);
-    }
-    if (path.section==1 && path.row==0) {
-        return CGRangeMake(60, 10);
-    }
-    if (path.row==2) {
-        return CGRangeMake(100, 0);
-    }
     return CGRangeZero;
 }
 
-#pragma mark 折线图专享功能
 
+#pragma mark 折线图专享功能
 //标记数值区域
 - (CGRange)UUChartMarkRangeInLineChart:(UUChart *)chart
 {
-    if (path.row==2) {
-        return CGRangeMake(25, 75);
-    }
     return CGRangeZero;
 }
+
 
 //判断显示横线条
 - (BOOL)UUChart:(UUChart *)chart ShowHorizonLineAtIndex:(NSInteger)index
@@ -204,9 +184,11 @@
     return YES;
 }
 
+
 //判断显示最大最小值
 - (BOOL)UUChart:(UUChart *)chart ShowMaxMinAtIndex:(NSInteger)index
 {
     return path.row==2;
 }
+
 @end
